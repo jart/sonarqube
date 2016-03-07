70a3996b00295f66136525331ea36eb7c73205b4
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-03-07 17:17:22 +0100

    SONAR-7426 Add new characteristic rating metrics

--- sonar-plugin-api/src/main/java/org/sonar/api/measures/CoreMetrics.java
+++ sonar-plugin-api/src/main/java/org/sonar/api/measures/CoreMetrics.java
@@ -2309,6 +2309,23 @@ public final class CoreMetrics {
     .setDeleteHistoricalData(true)
     .create();
 
+  /**
+   * @since 5.5
+   */
+  public static final String RELIABILITY_RATING_KEY = "reliability_rating";
+
+  /**
+   * @since 5.5
+   */
+  public static final Metric<Integer> RELIABILITY_RATING = new Metric.Builder(RELIABILITY_RATING_KEY, "Reliability Rating", Metric.ValueType.RATING)
+    .setDomain(DOMAIN_RELIABILITY)
+    .setDirection(Metric.DIRECTION_WORST)
+    .setQualitative(true)
+    .setBestValue(1.0)
+    .setWorstValue(5.0)
+    .create();
+
+
   // --------------------------------------------------------------------------------------------------------------------
   //
   // SECURITY CHARACTERISTIC
@@ -2347,6 +2364,22 @@ public final class CoreMetrics {
     .setBestValue(0.0)
     .setQualitative(true)
     .setDeleteHistoricalData(true)
+    .create();
+
+  /**
+   * @since 5.5
+   */
+  public static final String SECURITY_RATING_KEY = "security_rating";
+
+  /**
+   * @since 5.5
+   */
+  public static final Metric<Integer> SECURITY_RATING = new Metric.Builder(SECURITY_RATING_KEY, "Security Rating", Metric.ValueType.RATING)
+    .setDomain(DOMAIN_SECURITY)
+    .setDirection(Metric.DIRECTION_WORST)
+    .setQualitative(true)
+    .setBestValue(1.0)
+    .setWorstValue(5.0)
     .create();
 
   // --------------------------------------------------------------------------------------------------------------------
